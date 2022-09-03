499a247f9f1c65ef9afea61a60f977c461d0cab1
kevinalfito69 <kevinalfito69@gmail.com>
2022-09-03 21:01:46 +0700

    Link Component Navigation

--- hello-world/pages/index.js
+++ hello-world/pages/index.js
@@ -1,8 +1,19 @@
+import Link from 'next/link';
 const Home = () => {
     return (
         <>
             <h1>Home page</h1>
             <p>This is my first page</p>
+            <ul>
+                <li>
+                    <Link href="/blog">Blog</Link>
+                </li>
+                <li>
+                    <Link href="/product">Product</Link>
+                </li>
+                <li></li>
+                <li></li>
+            </ul>
         </>
     );
 };
